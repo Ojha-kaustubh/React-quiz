cceb6b88eed86a95d6f8b5a2cc42a5a10568858a
Ojha-kaustubh <kaustubha07ojha@gmail.com>
2025-04-05 23:03:38 +0530

    Create The Header Part

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+
+
+export default function Header () {
+    return (
+        <header className="app-header">
+            <img src="logo512.png" alt="React Image" />
+            <h1>The React Quiz</h1>
+        </header>
+    )
+}

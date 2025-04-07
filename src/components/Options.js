69d2a6e5fff3caf2bb16367f8d001408741206b9
Ojha-kaustubh <kaustubha07ojha@gmail.com>
2025-04-07 23:55:33 +0530

    display question and options

---
+++ src/components/Options.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Options = ({ question }) => {
+  return (
+    <div>
+      <div className="options">
+        {question.options.map((option) => (
+          <button key={option} className="btn btn-option">
+            {option}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Options;

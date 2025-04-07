56257d37dad6985244feb22792f3634a78345c5c
Ojha-kaustubh <kaustubha07ojha@gmail.com>
2025-04-07 23:40:21 +0530

    Starting the new Quiz

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const StartScreen = ( {numQuestions} ) => {
+const StartScreen = ( {numQuestions , dispatch} ) => {
   return (
     <div className="start">
       <h2>Welcome to the React Quiz</h2>
       <h3>{numQuestions} question to test your react mastery.</h3>
-      <button>Let's start</button>
+      <button className="btn btn-ui" onClick={ () => dispatch( {type: 'start'} )}>Let's start</button>
     </div>
   );
 };

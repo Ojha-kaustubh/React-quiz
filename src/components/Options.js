5164a253affb9f7373a9953d9d9c515261087882
Ojha-kaustubh <kaustubha07ojha@gmail.com>
2025-04-08 23:31:05 +0530

    Handling the answers

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,11 +1,21 @@
 import React from "react";
 
-const Options = ({ question }) => {
+const Options = ({ question, dispatch , answer}) => {
+
+  const hasAnswered = answer !== null;
+
   return (
     <div>
       <div className="options">
-        {question.options.map((option) => (
-          <button key={option} className="btn btn-option">
+        {question.options.map((option, index) => (
+          <button
+            key={option}
+            className={`btn btn-option 
+              ${ index === answer ? "answer" : ""}
+            ${hasAnswered && index === question.correctOption ? "correct" : "wrong"}`}
+            disabled={hasAnswered}
+            onClick={() => dispatch({ type: "newAnswer", payload: index })}
+          >
             {option}
           </button>
         ))}

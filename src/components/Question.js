29ba3305fb6beb4f322cb4078403c9e0993b6f2e
Ojha-kaustubh <kaustubha07ojha@gmail.com>
2025-04-08 23:31:17 +0530

    handling the questions

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Options from "./Options";
 
-const Question = ({ question }) => {
+const Question = ({ question , answer , dispatch}) => {
   console.log(question);
 
   return (
     <div>
       <h4>{question.question}</h4>
-      <Options question={question} />
+      <Options question={question}
+      dispatch={dispatch} answer={answer} />
     </div>
   );
 };

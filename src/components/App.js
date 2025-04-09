f91caa00bbd7a88b324c601ca2b70977bb794118
Ojha-kaustubh <kaustubha07ojha@gmail.com>
2025-04-09 23:46:49 +0530

    complete this project

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,11 @@ import Main from "./Main";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
+import NextButton from "./NextButton";
+import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
+import Footer from "./Footer";
+import Timer from "./Timer";
 
 const initialState = {
   questions: [],
@@ -13,6 +18,8 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0,
+  secondsRemaining: 10,
 };
 
 function reducer(state, action) {
@@ -23,23 +30,58 @@ function reducer(state, action) {
       return { ...state, status: "error" };
     case "start":
       return { ...state, status: "active" };
-    case "newAnswer":
-      return 
+    case "newAnswer": {
       const question = state.questions[state.index];
-      { ...state, answer: action.payload , points: action.payload === question.correctOption ? state.points + 1 : state.points};
-
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + 1
+            : state.points,
+      };
+    }
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
+    case "restart":
+      // return { ...initialState, questions: state.question, status: "ready" };
+      return {
+        ...state,
+        status: "ready",
+        index: 0,
+        answer: null,
+        points: 0,
+        highscore: 0,
+      };
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finised" : state.status,
+      };
     default:
       throw new Error("Unknown action type");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index ,answer}, dispatch] = useReducer(
-    reducer,
-    initialState
-  ); // Fixed destructuring
+  const [
+    { questions, status, index, answer, points, highscore, secondsRemaining },
+    dispatch,
+  ] = useReducer(reducer, initialState); // Fixed destructuring
 
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, curr) => prev + curr.points,
+    0
+  );
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
@@ -58,8 +100,39 @@ export default function App() {
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
 
-        {status === "active" && <Question question={questions[index]} 
-        dispatch={dispatch} answer = {answer}/>}
+        {status === "active" && (
+          <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              answer={answer}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+              maxPossiblePoints={maxPossiblePoints}
+            />
+            <Footer>
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
+              <NextButton
+                dispatch={dispatch}
+                answer={answer}
+                index={index}
+                numQuestions={numQuestions}
+              />
+            </Footer>
+          </>
+        )}
+
+        {status === "finished" && (
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
+            dispatch={dispatch}
+          />
+        )}
       </Main>
     </div>
   );
